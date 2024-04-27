0b658ff1ec9b949dd01d2babd583dcab1bbc43f8
CaiodaCosta <dacostacaio22ko@gmail.com>
2024-04-27 05:03:07 -0300

    Tamo ai mano treinamento 100%

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,29 @@
+import chalk from 'chalk';
+import fs from 'fs';
 import pegaArquivo from './index.js';
 //Essa é um arquivo  CLI, interface de linha de comando, criando um ponto de contato entre nossa biblioteca e o terminal de onde virão as informações.
 const caminho = process.argv;
-
-function processaTexto(caminho){
-    const resultado = pegaArquivo(caminho[2]);
-    console.log(resultado)
+function imprimeLista(resultado, identificador = ''){
+    console.log(
+        chalk.yellow('lista de links'),
+        chalk.black.bgGreen(identificador),
+        resultado);
+    
 }
+
+async function processaTexto(argumentos){
+    const caminho = argumentos[2];
+    
+    if (fs.lstatSync(caminho).isFile()){
+        const resultado = await pegaArquivo(argumentos);
+    } else if (fs.lstatSync(caminho).isDirectory){
+        const arquivos = await fs.promises.readdir(caminho)
+        arquivos.forEach(async (nomeDeArquivo) => {
+            const lista = await pegaArquivo(`${caminho}/${nomeDeArquivo}`)
+            imprimeLista(lista, nomeDeArquivo)
+        })
+       
+    }
+}
+
+processaTexto(caminho);
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import fs from 'fs';
 import chalk from 'chalk';
 function extraiLinks(texto){
-        const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
         const capturas = [...texto.matchAll(regex)]; //lê o texto e busca as informações de acordo com a expressao regular
-        const resultados = capturas.map(captura => ({[captura[1]] : captura[2]}));
-        return resultados;
+        const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}));
+        return resultados.length !== 0 ? resultados : "não há links no arquivo"
                     }
  
 
@@ -18,14 +18,12 @@ function trataErro(erro){
 async function pegaArquivo(caminhoDoArquivo){
     try {
     const encoding = 'utf-8';
-    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding);
-   return (texto);
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+   return extraiLinks(texto);
     } catch(erro) {
     trataErro(erro)
 }
 }
-
-pegaArquivo('./arquivos/texto.md')
 
 export default pegaArquivo;
 
