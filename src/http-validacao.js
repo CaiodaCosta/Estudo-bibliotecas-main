74cd3ba76bff64e49f5b2f2031f829f3707477cc
CaiodaCosta <dacostacaio22ko@gmail.com>
2024-04-30 02:06:47 -0300

    progresso

--- src/http-validacao.js
+++ src/http-validacao.js
@@ -1,3 +1,4 @@
+import chalk from "chalk"
 function extraiLinks(arrLinks) {
     return arrLinks.map((objetoLink) => Object.values(objetoLink).join());
     //nessa função extraiLinks ela mesma retorna valores do arrLinks, mapeia todos os arrays do parametro
@@ -8,8 +9,13 @@ async function checaStatus(listaURLs) {
     const arrStatus = await Promise
         .all(
             listaURLs.map(async (url) => {
-                const response = await fetch(url)
-                return response.status;
+                try{
+                    const response = await fetch(url)
+                    return response.status;
+
+                } catch(erro){
+                    return manejaErros(erro);
+                }
 
                 //Promise.all retorna uma unica promise que resolve todas as promises pendentes
                 //no caso abaixo o fetch só lida com um recurso por vez
@@ -19,9 +25,21 @@ async function checaStatus(listaURLs) {
         )
     return arrStatus
 }
+
+function manejaErros (erro){
+    if (erro.cause.code === 'ENOTFOUND'){
+        return 'link não encontrado';
+    } else {
+        return 'Ocorreu algum erro';
+    }
+}
 export default async function listaValidada(listaDeLinks) {
     const links = extraiLinks(listaDeLinks);
     const status = await checaStatus(links);
     
-    return status;
+    return listaDeLinks.map((objeto, indice) => ({
+        ...objeto,
+        status: status[indice]
+    }) )
+    
 }
